19a794fb26fefe6dc868442a8a4927e625b72c25
tracktwo <jcemmett1@gmail.com>
2023-02-03 09:11:03 -0500

    Add rust tools

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -176,3 +176,16 @@ null_ls.setup({
   }
 })
 
+-- rust-tools
+local rt = require('rust-tools')
+
+rt.setup({
+  server = {
+    on_attach = function(client, bufnr)
+      on_attach(client, bufnr)
+      vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+      -- Code action groups
+      vim.keymap.set("n", "<Leader>ca", rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+  },
+})
--- lua/jonathan/packer.lua
+++ lua/jonathan/packer.lua
@@ -86,6 +86,8 @@ require('packer').startup(function(use)
 
   use { 'glts/vim-radical', requires = { "glts/vim-magnum" } }
 
+  use { 'simrat39/rust-tools.nvim' }
+
   -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
