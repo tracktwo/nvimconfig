ae17297d5564fd391799b7fe4d0af1a344c3eb9f
tracktwo <jcemmett1@gmail.com>
2023-02-26 20:30:16 -0500

    Other dap keybinds

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -19,11 +19,14 @@ vim.keymap.set('n', '<F11>', dap.step_into, { desc = "[D]AP: Step [I]nto" })
 vim.keymap.set('n', '<F10>', dap.step_over, { desc = "[D]AP: Step [O]ver" })
 -- F23 == S-F11
 vim.keymap.set('n', '<F23>', dap.step_out, { desc = "[D]AP: Step O[u]t" })
+vim.keymap.set('n', '<S-F11>', dap.step_out, { desc = "[D]AP: Step O[u]t" })
 -- F22 == S-F10
 vim.keymap.set('n', '<F22>', dap.run_to_cursor, { desc = "[D]AP: Run to [C]ursor" })
+vim.keymap.set('n', '<S-F10>', dap.run_to_cursor, { desc = "[D]AP: Run to [C]ursor" })
 vim.keymap.set('n', '<F5>', dap.continue, { desc = "[D]AP: [G]o" })
 -- F17 == S-F5
 vim.keymap.set('n', '<F17>', dap.terminate, { desc = "[D]AP: Close" })
+vim.keymap.set('n', '<S-F5>', dap.terminate, { desc = "[D]AP: Close" })
 
 vim.keymap.set('n', '<leader>dU', dapui.toggle, { desc = "[D]AP: Toggle [U]I" })
 vim.keymap.set('n', '<leader>dc', dap.focus_frame, { desc = "[D]AP: Fo[c]us Frame" })
