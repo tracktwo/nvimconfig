74e3f752625808e8dded2432d5259a98dfc1c7c6
tracktwo <jcemmett1@gmail.com>
2023-02-26 22:13:13 -0500

    change incremental selection keybind

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -9,8 +9,8 @@ require('nvim-treesitter.configs').setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = '<c-space>',
-      node_incremental = '<c-space>',
+      init_selection = '<c-b>',
+      node_incremental = '<c-b>',
       scope_incremental = '<c-s>',
       node_decremental = '<c-backspace>',
     },
