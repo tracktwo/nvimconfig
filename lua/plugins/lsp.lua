2e231d97ab89f2d00f35a543f29c7567e2bec36c
Jonathan Emmett <jcemmett1@gmail.com>
2023-10-31 09:11:33 -0300

    rust tools

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,7 +7,8 @@ return {
             'williamboman/mason-lspconfig.nvim',
             'j-hui/fidget.nvim',
             'folke/neodev.nvim',
-            'L3MON4D3/LuaSnip'
+            'L3MON4D3/LuaSnip',
+            'simrat39/rust-tools.nvim',
         },
         config = function()
             local lsp_zero = require('lsp-zero')
@@ -19,7 +20,24 @@ return {
             require('mason-lspconfig').setup({
                 ensure_installed = {},
                 handlers = {
-                    lsp_zero.default_setup
+                    lsp_zero.default_setup,
+                    rust_analyzer = function()
+                        local rust_tools = require('rust-tools')
+                        rust_tools.setup({
+                            server = {
+                                on_attach = function(client, bufnr)
+                                    vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, {buffer=bufnr})
+                                    vim.keymap.set('n', '<leader>ca', rust_tools.code_action_group.code_action_group, {buffer=bufnr})
+                                end,
+                                settings = {
+                                    ["rust-analyzer"] = {
+                                        procMacro = { enable = true },
+                                        check = { command = "clippy" },
+                                    }
+                                }
+                            }
+                        })
+                    end
                 }
             })
         end
