2688392c3f25af84b25cd17eee2ef0b7e9fc3037
tracktwo <jcemmett1@gmail.com>
2023-11-11 10:47:35 -0400

    add treesitter

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,4 +46,30 @@ return {
             require('cmp').setup({})
         end
     },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ":TSUpdate",
+        config = function()
+            local configs = require('nvim-treesitter.configs')
+            configs.setup({
+                ensure_installed = { "lua", "vim", "vimdoc", "rust" },
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = true
+                },
+                indent = {
+                    enable = true
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "gnn",
+                        node_incremental = "grn",
+                        scope_incremental = "grc",
+                        node_decremental = "grm"
+                    }
+                }
+            })
+        end
+    }
 }
