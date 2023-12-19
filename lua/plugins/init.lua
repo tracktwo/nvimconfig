43846892d52396889daf4236cacf888ace74882a
tracktwo <jcemmett1@gmail.com>
2023-12-19 19:16:31 -0400

    update & add hop

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,4 +77,16 @@ return {
             require('symbols-outline').setup()
         end
     }
+    ,
+    {
+        'smoka7/hop.nvim',
+        version="*",
+        opts = {},
+        keys = {
+            { 'f', function() require('hop').hint_char1({direction=require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true}) end }
+            { 'F', function() require('hop').hint_char1({direction=require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true}) end }
+            { 't', function() require('hop').hint_char1({direction=require('hop.hint').HintDirection.AFTER_CURSOR, current_line_only = true, hint_offset=-1}) end }
+            { 'T', function() require('hop').hint_char1({direction=require('hop.hint').HintDirection.BEFORE_CURSOR, current_line_only = true, hint_offset=-1}) end }
+        }
+    }
 }
